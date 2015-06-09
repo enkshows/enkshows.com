f5d6c45db7b32ad8873b6741fe7abb28b7a01091
Justin Cibrone <jcibrone@advanstar.com>
2015-06-09 10:55:27 -0700

    uploading index.php

--- index.php
+++ index.php
@@ -1,60 +1,50 @@
 <?php require('bin/ClassMathGuard.php');?>
-<?php require('includes/main-header.php');?>
+<?php include("includes/variables.php"); ?>
+<?php include("includes/_v2/main-header-speed-test.php"); ?>
+
+ 
 <body id="home" class="homePage">
 <div id="wrapper">
 
-	<!-- Navigation-->	
-    <div id="nav-col">
-    	<div id="nav">
-
-            <?php include('includes/main-nav.php'); ?>
-            
-            <?php include('includes/main-nav-social.php'); ?>
-         
-        </div>
-    </div> <!-- End Navigation-->
+  <!-- Navigation-->  
+    <?php include("includes/_v2/v2-main-nav.php"); ?>
     
-    <!-- Content -->	
+    <!-- Content--> 
   <div id="main-col">
-
-      <?php include("includes/main/main-postano.php"); ?>
-      <!-- Postano -->
     
-      <div class="module" id="show-dates">
-          <?php include("includes/main/calendar-header.php"); ?>
-          <p>hello</p>
-          <?php include("includes/_v2/v2-main-show-tiles.php"); ?>
-      </div>
-      <!-- Show Dates / Calendar -->
+      <!-- Calendar -->
+        <div class="module" id="show-dates">
+            <?php include("includes/main/calendar-header.php"); ?>
+            <!-- Show Col Left -->
+            <?php //include("includes/main/main-show-tiles.php"); ?>
+            <?php include("includes/_v2/v2-main-show-tiles.php"); ?>
+        </div>
+      
 
-
-  <!-- Attend -->
-  <?php include("includes/main/main-attend.php"); ?>
-
-  <!-- About -->
-	<?php include("includes/main/main-about.php"); ?>
-	
-	<!-- Exhibit -->
-	<?php include("includes/main/main-exhibit.php"); ?>
-	
-	<!-- Press -->
-	<?php include("includes/main/main-press.php"); ?>
-  
-  	<!-- Travel -->
-	<?php include("includes/main/main-travel.php"); ?>
-	
-	<!-- Contact -->
-	<?php include("includes/main/main-contact.php"); ?>
-
+        <!-- Attend -->
+        <?php include("includes/_v2/v2-main-attend.php"); ?>
     
-
-    	<!-- BLANK SPACE-->
-    	<div style="float:left; clear:both; height:600px;" ></div>
-        <!-- BLANK SPACE-->
+    <!-- About -->
+    <?php include("includes/_v2/v2-main-about.php"); ?>
+    
+    
+    
+    <!-- Exhibit -->
+        <?php include("includes/_v2/v2-main-exhibit.php"); ?>
         
+    <!-- Press -->
+        <?php include("includes/_v2/v2-main-press.php"); ?>  
+        
+        <!-- Travel -->
+        <?php include("includes/_v2/v2-main-travel.php"); ?> 
+        
+    <!-- Contact -->   
+    <?php include("includes/main/main-contact.php"); ?>
+
     </div><!-- end main col-->
 </div><!-- end wrapper-->
 
-	<!-- Footer -->
-	<?php include("includes/main-footer-2.php"); ?>
 
+
+
+<?php include("includes/_v2/v2-footer.php"); ?>
