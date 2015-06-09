046f111500b03e4dc5bc89e35104b8420efcf055
Justin Cibrone <jcibrone@advanstar.com>
2015-06-09 10:26:38 -0700

    adding new include files

---
+++ includes/main-nav-fwd-social .php
@@ -0,0 +1,6 @@
+<ul id="social" class="socialNav"> 
+  <li><a id="calendar" target="_blank" href="http://www.enkshows.com/company/calendar.html" title="All Shows Calendar"><span>http://www.enkshows.com/company/calander.html</span></a></li>
+  <li><a id="fb" target="_blank" href="https://www.facebook.com/ENKInternational" title="Facebook"><span>https://www.facebook.com/ENKInternational</span></a></li>
+  <li><a id="twit" target="_blank" href="https://twitter.com/ENKSHOWS" title="Twitter"><span>hhttps://twitter.com/ENKSHOWS</span></a></li>
+  <li><a id="instagram" target="_blank" href="https://instagram.com/fwdshow/" title="Instagram"><span>https://instagram.com/fwdshow/</span></a></li>
+</ul>
